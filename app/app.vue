b075fa02372703e5a7c22f316f7aeaee607fc20e
Jowohny <vujohny109238@gmail.com>
2025-09-19 20:49:09 -0700

    basic layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,28 @@
+<script setup lang="ts">
+
+let boxCount: number = 10
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <div class="flex content-center justify-center m-4 flex-col">
+      <div>
+        <img class="w-1/2 justify-self-center" src="/image0.jpg">
+        <h1 class="text-4xl font-thin tracking-widest m-4 justify-self-center">Inventory</h1>
+      </div>
+      <div class="grid grid-cols-2 gap-4 mb-8">
+        <div class="text-center" v-for="_ in boxCount">
+          <img src="/box.jpg">
+        </div>
+      </div>
+
+      <UDrawer :handle="false" should-scale-background>
+        <UButton class="inline" label="View Total Inventory" color="neutral" variant="subtle"/>
+          <template #content>
+            <div>bean jar</div>
+          </template>
+      </UDrawer>
+    </div>
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,21 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/content',
+    '@nuxt/ui'
+  ]
 });
